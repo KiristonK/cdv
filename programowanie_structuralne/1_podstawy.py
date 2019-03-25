bcf2ea60f45bd739d66f11590f6467cc6fa4e672
Vadym Kostyrkin <vkostyrkin@edu.cdv.pl>
2019-03-25 15:28:31 +0100

    podstawy

---
+++ programowanie_structuralne/1_podstawy.py
@@ -0,0 +1,28 @@
+#print("CDV")
+#print(2)
+
+#potęga
+#potega = 2 ** 10
+#print(potega)
+
+#pobieranie danuch z klawiatury
+#imie = input();
+#konkatenacja +
+#print("Twoje imie podane z klawiatury : " + imie);
+
+#nazwisko = input()
+#print("Twoje imje : " + imie + ", Twoje nazwisko : " + nazwisko)
+
+#dlugosc = len(nazwisko)
+#print(type(dlugosc))
+#print("Dlugosc nazwiska : ", dlugosc)
+
+#dlugosc = str(dlugosc)
+
+#print(type(dlugosc))
+#print("Dlugosc nazwiska : " + dlugosc)
+print("\nPodaj swoj wiek : ", end="")
+
+wiek = input();
+wiek = str(wiek)
+print("Twoj wiek jest : " + wiek + " lat")
