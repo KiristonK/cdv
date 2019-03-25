4a9f1f81673c19db598d2aaeb678c78e7d23428d
Vadym Kostyrkin <vkostyrkin@edu.cdv.pl>
2019-03-25 15:58:17 +0100

    python

--- programowanie_structuralne/1_podstawy.py
+++ programowanie_structuralne/1_podstawy.py
@@ -24,5 +24,42 @@
 print("\nPodaj swoj wiek : ", end="")
 
 wiek = input();
+print("Twoj wiek jest : ", wiek, " lat")
+
+nazwisko = 'Kowalski'
+#imie_zmiennejNastepne_slowo_z_duzej to camelcase
+pierwszyZnak = nazwisko[0]
+#ostatniZnak = nazwisko[len(nazwisko)-1]
+ostatniZnak = nazwisko[-1]
+print(pierwszyZnak ,nazwisko[1], ostatniZnak)
+
+
+#kowersja
+
+x = "5"
+print(type(x))
+x = int(x)
+print(type(x))
+
+text = "Jan" * 2
+print(type(text))
+
+y = 6
+print(type(y))
+y = y / 2
+print(type(y)) #float
+print(y)
+
+wiek = 21
+print("Twoj wiek :", wiek)
 wiek = str(wiek)
-print("Twoj wiek jest : " + wiek + " lat")
+print("Twoj wiek : " + wiek)
+
+nazwisko = "Jankowski"
+print(nazwisko[0])
+print(nazwisko[0 : 3])
+print(nazwisko[-2])
+print(nazwisko[-2 :])
+print(nazwisko[: -2])
+print(nazwisko[:-2:2]) #J(a)n(k)o(w)s(k)i
+print(nazwisko[: : 2])
