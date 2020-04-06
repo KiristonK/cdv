fd02e6a0a3e242829cb8c4e45e19dbfd9d51ba78
Vadym Kostyrkin <vkostyrkin@edu.cdv.pl>
2020-04-06 12:57:55 +0200

    Init Projekt

---
+++ technologie_internetowe/ProjektKalendar/main.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+    <title>Kalendar</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap-grid.css">
+    <link rel="stylesheet" href="css/bootstrap-reboot.css">
+</head>
+
+<body>
+<div class="row">
+    <div class="col-3" style="border: 1px solid #000000;">
+        <h1 class="display-4">Start</h1>
+    </div>
+    <div class="col-6" style="border: 1px solid #000000;">
+        <h1 class="display-4">Middle</h1>
+    </div>
+    <div class="col-3" style="border: 1px solid #000000;">
+        <h1 class="display-4">End</h1>
+    </div>
+</div>
+<div class="row">
+    <div class="col-3" style="border: 1px solid #000000;">
+    </div>
+    <div class="col-9" style="border: 1px solid #000000;">
+        <?php
+
+        ?>
+    </div>
+</div>
+<div class="row">
+
+</div>
+</body>
+
+<link rel="script" href="js/bootstrap.bundle.js">
+<link rel="script" href="js/bootstrap.js">
+</html>
+
