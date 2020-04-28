10f02b8a96098167f508761882fec2f3cbe46c2e
KiristonK <kiriston.k@gmail.com>
2020-04-28 17:54:21 +0200

    ...

---
+++ technologie_internetowe/lessons/PHP/DataBase/connect.php
@@ -0,0 +1,3 @@
+<?php
+$conn = new mysqli("127.0.0.1:8080", "root", "root");
+?>
---
+++ technologie_internetowe/lessons/PHP/DataBase/database_1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+</head>
+<body>
+<h3>Table user</h3>
+<?php
+?>
+</body>
+</html>
